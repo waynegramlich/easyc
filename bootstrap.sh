011b8666553176753fd6d4bade5d64642bede259
Wayne C. Gramlich <Wayne@Gramlich.Net>
2013-03-05 14:30:16 -0800

    Force bootstrap.sh to compile the final compiler.

--- bootstrap.sh
+++ bootstrap.sh
@@ -15,4 +15,7 @@ gcc -g -c -I. Token.ez.c
 gcc -g -c -I. Unix.ez.c
 gcc -g -c -o Easy_C_C.ez.o -I. Easy_C_C.c
 gcc -g -c -o Unix_C.ez.o -I. Unix_C.c
-gcc -g -o ezcc.ez ezcc.ez.c EZCC.ez.o Compiler.ez.o Easy_C.ez.o Declaration.ez.o Token.ez.o Parse.ez.o Expression.ez.o Statement.ez.o Unix.ez.o Easy_C_C.ez.o Unix_C.ez.o
+echo Linking Bootstrap Compiler
+gcc -g -o ezcc ezcc.ez.c EZCC.ez.o Compiler.ez.o Easy_C.ez.o Declaration.ez.o Token.ez.o Parse.ez.o Expression.ez.o Statement.ez.o Unix.ez.o Easy_C_C.ez.o Unix_C.ez.o
+echo Using bootstrap to compile up new compiler.
+./ezcc -S .ez -o ezcc.ez EZCC
